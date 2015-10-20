75a0c68e7ff4af5f926807f8a8aa0eb0848e63ff
Katsuyuki Kato <wk_bad.9ball@docomo.ne.jp>
2015-10-20 10:59:30 +0900

    基礎課題

---
+++ src/KisoKadai3.java
@@ -0,0 +1,8 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+public class KisoKadai3 {
+	public static void main(String[] args){
+
+}
